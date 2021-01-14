915cc0022456b814e05a27aecc6af1df9148149f
lucky <lmnarasimha2301@gmail.com.com>
2021-01-14 12:03:03 +0530

    yumsto deployed2

--- src/Pages/Routes.js
+++ src/Pages/Routes.js
@@ -9,10 +9,10 @@ import Home from './Home'
 const Routes= ()=>{
     return(
         <BrowserRouter >
-            <Route path={'/'} exact component={Home} />
-            <Route path={'/login'} exact component={LoginPage} />
-            <Route path={'/addmeal'} exact component={AddingMeal} />
-            <Route path={'/searchresults'} exact component={SearchResults} />
+            <Route path={'/yumsto-deploy'} exact component={Home} />
+            <Route path={'/yumsto-deploy/login'} exact component={LoginPage} />
+            <Route path={'/yumsto-deploy/addmeal'} exact component={AddingMeal} />
+            <Route path={'/yumsto-deploy/searchresults'} exact component={SearchResults} />
         </BrowserRouter>
     )
     
